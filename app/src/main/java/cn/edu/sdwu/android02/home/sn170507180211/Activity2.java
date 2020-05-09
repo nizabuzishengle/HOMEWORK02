ed6f9ad6d38c10c8aa4efa5fd7a1444b9ba5e254
nizabuzishengle <1726356615@qq.com>
2020-05-09 16:04:38 +0800

    作业三

---
+++ app/src/main/java/cn/edu/sdwu/android02/home/sn170507180211/Activity2.java
@@ -0,0 +1,12 @@
+package cn.edu.sdwu.android02.home.sn170507180211;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/cn/edu/sdwu/android02/home/sn170507180211/MainActivity.java
+++ app/src/main/java/cn/edu/sdwu/android02/home/sn170507180211/MainActivity.java
@@ -1,13 +1,29 @@
 package cn.edu.sdwu.android02.home.sn170507180211;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public  void work1(View view){
+        Intent intent=new Intent(this,Activity1.class);
+        startActivity(intent);
+    }
+    public  void work2(View view){
+        Intent intent=new Intent(this,Activity2.class);
+        startActivity(intent);
+    }
+    public void work3(View view){
+        Intent intent=new Intent(this,Activity3.class);
+        startActivity(intent);
+    }
+
 }
